6ebf68308ab8c381ef6f76d21b5c27aca8ec255a
Arslonbek <roziboyevarslon0110@gmail.com>
2025-08-20 14:30:20 +0500

    middleware changed

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -1,33 +1,14 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  const router = useRouter();
+ const router = useRouter();
   const route = router.resolve(to.path);
-  const accessToken = useCookie('access_token');
 
-  if (!from.path.includes("/auth/register") && to.path.includes("/auth/register")) {
-    return navigateTo("/auth/register");
-  }
-
-  if (!accessToken.value && !to.path.includes("/auth/login") && !to.path.includes("/auth/register")) {
-    return navigateTo("/auth/login");
-  }
-
-  // If it's the home path, redirect to /overview
+  // Home sahifadan listening sahifasiga redirect
   if (to.path === '/') {
-    return navigateTo('/carriers');
-  }
-
-  // Redirect to / carrier if carrierId is missing and not on excluded routes
-  const excludedPaths = ["/auth/login", "/auth/register", "/carriers", "/clients"];
-  if (!excludedPaths.includes(to.path) && !getCarrierId()) {
-    return navigateTo("/carriers");
-  }
-
-  if (["/carriers", "/clients"].includes(to.path) && getCarrierId()) {
-    return navigateTo("/listening");
+    return navigateTo('/listening');
   }
 
   // Check if the route exists
-  if (!route.matched.length) {
-    return abortNavigation();
-  }
+  // if (!route.matched.length) {
+  //   return abortNavigation();
+  // }
 });
