02f9d551c03553e3417b44128b189a844517dba6
Arslonbek <roziboyevarslon0110@gmail.com>
2025-08-21 11:11:52 +0500

    update button title

--- pages/listening/index.vue
+++ pages/listening/index.vue
@@ -1,14 +1,16 @@
 <template>
 	<main>
-		<AudioUpload v-model="statusModal" @upload="handleFileUpload" :loading="audioUpload"/>
+		<AudioUpload v-model="statusModal" @upload="handleFileUpload" :loading="audioUpload" />
 		<section class="card flex justify-between items-center">
 			<h1 class="title">Listening</h1>
-			<UButton variant="ghost" @click="statusModal = true">Open Modal</UButton>
-			<UInput class="min-w-[264px]" v-model="search" :ui="{
-				base: 'placeholder:font-medium',
-				rounded: 'rounded-lg',
-				color: { white: { outline: 'shadow-none ring-grey-border' } }
-			}" color="white" icon="i-heroicons-magnifying-glass-20-solid" size="lg" placeholder="Search listening" />
+			<div class="flex items-center gap-3">
+				<UButton variant="solid" size="sm" label="Upload Audio" @click="statusModal = true" />
+				<UInput class="min-w-[264px]" v-model="search" :ui="{
+					base: 'placeholder:font-medium',
+					rounded: 'rounded-lg',
+					color: { white: { outline: 'shadow-none ring-grey-border' } }
+				}" color="white" icon="i-heroicons-magnifying-glass-20-solid" size="lg" placeholder="Search listening" />
+			</div>
 		</section>
 		<section class="grid grid-cols-12 gap-x-1.5 mt-1.5">
 			<!-- Your content here -->
