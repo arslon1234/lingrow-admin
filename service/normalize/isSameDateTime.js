22912114d7dc3455b0ae5a603db91929f5bdb15b
Arslonbek Ro'ziboyev <roziboyevarslon0110@gmail.com>
2025-12-23 20:57:08 +0500

    remove unnessary files

--- service/normalize/isSameDateTime.js
+++
@@ -1,5 +0,0 @@
-import dayjs from 'dayjs';
-
-export function isSameDateTime(event1, event2) {
-	return dayjs(dayjs(event1.dateTime).format('YYYY-MM-DDTHH:mm:ss.SSS')).isSame(dayjs(event2.dateTime).format('YYYY-MM-DDTHH:mm:ss.SSS'));
-}
